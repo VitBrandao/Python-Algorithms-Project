bc359de10ebb9a33a4e6a739ebfda45e3d8f6ced
vitor <vitorsbsilva@gmail.com>
2022-09-01 10:58:26 -0300

    fix: flake8 function complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,18 +1,23 @@
-def study_schedule(permanence_period, target_time):
-    if (permanence_period is None or target_time is None):
-        return None
-
+def count_student_hours(permanence_period, target_time):
     counter = 0
     for period in permanence_period:
         for item in period:
             if (item is None or type(item) != int):
                 return None
-        if(period[0] <= target_time <= period[1]):
+        if (period[0] <= target_time <= period[1]):
             counter += 1
     return counter
 
 
+def study_schedule(permanence_period, target_time):
+    if (target_time is None):
+        return None
+
+    count_hours = count_student_hours(permanence_period, target_time)
+    return count_hours
+
+
 # estudante             1       2       3       4       5       6
-# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
 # permanence_periods = [(4, None), ("0", 4)]
-# print(study_schedule(permanence_periods, 5))
+print(study_schedule(permanence_period, 5))
