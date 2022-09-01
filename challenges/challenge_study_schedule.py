a06e19fd70dd6d59e2cec26dedca6014a617275b
vitor <vitorsbsilva@gmail.com>
2022-09-01 10:50:54 -0300

    feat: (req.1) challenge_study_schedule to find which hour most students are studying

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,18 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui!!"""
+    if (permanence_period is None or target_time is None):
+        return None
+
+    counter = 0
+    for period in permanence_period:
+        for item in period:
+            if (item is None or type(item) != int):
+                return None
+        if(period[0] <= target_time <= period[1]):
+            counter += 1
+    return counter
+
+
+# estudante             1       2       3       4       5       6
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# permanence_periods = [(4, None), ("0", 4)]
+# print(study_schedule(permanence_periods, 5))
