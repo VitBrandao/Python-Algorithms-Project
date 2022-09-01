4c7844e3742c42d1e2bfba123141fd93798092ec
vitor <vitorsbsilva@gmail.com>
2022-09-01 13:28:17 -0300

    feat: (req.3) challenge_anagrams to check if received words are anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,28 @@
+def order_string(string):
+    string_list = list(string)
+    str_len = len(string)
+
+    for index in range(1, str_len):
+        key = string_list[index]
+        new_position = index - 1
+
+        while new_position >= 0 and string_list[new_position] > key:
+            string_list[new_position + 1] = string_list[new_position]
+            new_position = new_position - 1
+
+        string_list[new_position + 1] = key
+
+    return string_list
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    low_first_string = first_string.lower()
+    low_second_string = second_string.lower()
+
+    order_first = order_string(low_first_string)
+    order_second = order_string(low_second_string)
+
+    return order_first == order_second
+
+
+print(is_anagram('Amor', 'rOmA'))
