b48472766fee78f10ea122cd2894a533d7c642c2
vitor <vitorsbsilva@gmail.com>
2022-09-01 12:30:19 -0300

    feat: (req.2) challenge_palindromes_recursive to check if word received as parameter is palindrome or not

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,26 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    lower_word = word.lower()
+
+    if (len(lower_word) == 0):
+        return False
+    elif (len(lower_word) == 1 or (len(lower_word) == 2 and lower_word[0] == lower_word[1])):
+        return True
+    else:
+        if lower_word[low_index] == lower_word[high_index]:
+            rest_of_the_word = lower_word[1:-1]
+            new_higher_index = len(rest_of_the_word) - 1
+
+            return is_palindrome_recursive(rest_of_the_word, 0, new_higher_index)
+        else:
+            return False
+
+
+# word = 'Anana'
+# print(is_palindrome_recursive(word, 0, len(word) - 1))
+
+# O algoritmo deve ser feito utilizando a solução recursiva;
+
+# Não se preocupe com a análise da complexidade desse algoritmo;
+
+# Se for passado uma string vazia, retorne False;
+
