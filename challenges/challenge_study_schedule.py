57b4dfa889278b94f4797e22d2598bf57315e76a
vitor <vitorsbsilva@gmail.com>
2022-09-01 11:24:32 -0300

    refactor: challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,23 +1,21 @@
-def count_student_hours(permanence_period, target_time):
+def study_schedule(permanence_period, target_time):
     counter = 0
-    for period in permanence_period:
-        for item in period:
-            if (item is None or type(item) != int):
-                return None
-        if (period[0] <= target_time <= period[1]):
+    if (target_time is None):
+        return None
+
+    for entry, leave in permanence_period:
+        not_valid_entry = entry is None or type(entry) != int
+        not_valid_leave = leave is None or type(leave) != int
+
+        if (not_valid_entry or not_valid_leave):
+            return None
+
+        if (entry <= target_time <= leave):
             counter += 1
     return counter
 
 
-def study_schedule(permanence_period, target_time):
-    if (target_time is None):
-        return None
-
-    count_hours = count_student_hours(permanence_period, target_time)
-    return count_hours
-
-
 # estudante             1       2       3       4       5       6
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
 # permanence_periods = [(4, None), ("0", 4)]
-print(study_schedule(permanence_period, 5))
+# print(study_schedule(permanence_periods, 5))
